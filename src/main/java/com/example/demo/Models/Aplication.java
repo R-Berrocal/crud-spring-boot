2f86075c31b91d3d74d0a7793164df3713fb2ab0
Roberto Berrocal <robertoberro20@gmail.com>
2023-04-27 12:16:51 -0500

    removing innecesary imports

--- src/main/java/com/example/demo/Models/Aplication.java
+++ src/main/java/com/example/demo/Models/Aplication.java
@@ -1,8 +1,6 @@
 package com.example.demo.Models;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonProperty.Access;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
--- src/main/java/com/example/demo/Models/Server.java
+++ src/main/java/com/example/demo/Models/Server.java
@@ -2,10 +2,8 @@ package com.example.demo.Models;
 
 import java.util.List;
 
-import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import com.fasterxml.jackson.annotation.JsonProperty.Access;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 
